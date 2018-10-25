91385a12f39d472bc6db143e78e7919289a1831b
Michael Bukachi <michaelbukachi@gmail.com>
2018-10-25 15:33:35 +0300

    Added test code

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,12 @@
 import pytest
+import requests
 
 
 @pytest.fixture(scope='module')
 def base():
-    return ''
+    return 'http://0.0.0.0:5000'
+
+
+@pytest.fixture(scope='module')
+def client():
+    yield requests
--- test/test_example.py
+++ test/test_example.py
@@ -1,2 +1,4 @@
-def test_home_page_is_shown(base):
-    assert True
+def test_home_page(base, client):
+    res = client.get(base)
+    assert res.ok
+    assert 'What\'s up Docker Deep Divers! You\'ve visited me' in str(res.content)
