f007e200a42a161793621092b1a0b0d9136be281
Michael Bukachi <michaelbukachi@gmail.com>
2018-10-25 15:57:45 +0300

    Add server wait snippet in conftest

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,32 @@
+import socket
+import subprocess
+import time
+
 import pytest
 import requests
+
+
+@pytest.fixture(scope='session')
+def server():
+    timeout = 5
+    subprocess.call(['docker-compose', '-f', 'counter-app/docker-compose.yml', 'up', '-d'])
+    count = 0
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while count < timeout:
+        print('Checking server...')
+        result = sock.connect_ex(('0.0.0.0', 5000))
+        if result == 0:
+            print('Ready...')
+            break
+
+        time.sleep(1)
+        count += 1
+
+    if count == timeout:
+        raise pytest.exit('Unable to connect to running app')
+    yield
+
+    subprocess.call(['docker-compose', '-f', 'counter-app/docker-compose.yml', 'down'])
 
 
 @pytest.fixture(scope='module')
@@ -8,5 +35,5 @@ def base():
 
 
 @pytest.fixture(scope='module')
-def client():
+def client(server):
     yield requests
